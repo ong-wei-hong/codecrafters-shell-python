f6a983347f4e828a07f242b886f43b407ba81877
ong-wei-hong <weihong.ong@outlook.com>
2024-12-10 22:15:49 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
 import sys
 
+def get_user_command():
+    sys.stdout.write("$ ")
+    inp = input()
+    return inp
+
+def handle_command(inp):
+    print(f'{inp}: command not found')
 
 def main():
     # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-
     # Wait for user input
-    input()
+    inp = get_user_command()
+    handle_command(inp)
+
 
 
 if __name__ == "__main__":
