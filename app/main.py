4827a1a51c8b143071c0dd4c11a120ff09d45954
ong-wei-hong <weihong.ong@outlook.com>
2024-12-12 00:49:17 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 import sys
 import os
+import subprocess
 
 def get_user_command():
     sys.stdout.write("$ ")
     inp = input().split(' ')
     return inp
+
+def get_file(dirs, filename):
+    for dir in dirs:
+        filepath = f'{dir}/{filename}'
+        if os.path.isfile(filepath):
+            return filepath
+    return None
 
 def handle_command(inp,dirs):
     match inp:
@@ -17,17 +25,16 @@ def handle_command(inp,dirs):
         case ['type', arg]:
             if arg in ['type', 'exit', 'echo']:
                 sys.stdout.write(f'{arg} is a shell builtin\n')
+            elif (filepath := get_file(dirs, arg)):
+                sys.stdout.write(f'{arg} is {filepath}\n')
             else:
-                for dir in dirs:
-                    filepath = f'{dir}/{arg}'
-                    if os.path.isfile(filepath):
-                        sys.stdout.write(f'{arg} is {filepath}\n')
-                        return
-                
                 sys.stdout.write(f'{arg}: not found\n')
         
-        case _:
-            sys.stdout.write(f'{' '.join(inp)}: command not found\n')
+        case [file, *args]:
+            if (filepath := get_file(dirs, file)):
+                subprocess.run([filepath, *args])
+            else:
+                sys.stdout.write(f'{' '.join(inp)}: command not found\n')
 
 def main(dirs):
     # Uncomment this block to pass the first stage
