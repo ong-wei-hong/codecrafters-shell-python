d2bb67e09b6cb87176832263b59449c034135866
ong-wei-hong <weihong.ong@outlook.com>
2024-12-12 22:15:26 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ def get_file(dirs, filename):
             return filepath
     return None
 
-def handle_command(inp,dirs):
+def handle_command(inp,dirs,curr_dir):
     match inp:
         case ['exit', '0']:
             sys.exit(0)
@@ -23,12 +23,15 @@ def handle_command(inp,dirs):
             sys.stdout.write(' '.join(args) + '\n')
 
         case ['type', arg]:
-            if arg in ['type', 'exit', 'echo']:
+            if arg in ['type', 'exit', 'echo', 'pwd']:
                 sys.stdout.write(f'{arg} is a shell builtin\n')
             elif (filepath := get_file(dirs, arg)):
                 sys.stdout.write(f'{arg} is {filepath}\n')
             else:
                 sys.stdout.write(f'{arg}: not found\n')
+
+        case ['pwd']:
+            sys.stdout.write(f'{curr_dir}\n')
         
         case [file, *args]:
             if (filepath := get_file(dirs, file)):
@@ -36,14 +39,15 @@ def handle_command(inp,dirs):
             else:
                 sys.stdout.write(f'{' '.join(inp)}: command not found\n')
 
-def main(dirs):
+def main(dirs, curr_dir):
     # Uncomment this block to pass the first stage
     # Wait for user input
     while True:
         inp = get_user_command()
-        handle_command(inp,dirs)
+        handle_command(inp,dirs,curr_dir)
 
 if __name__ == "__main__":
     PATH = os.environ.get("PATH")
     dirs = PATH.split(':')
-    main(dirs)
+    curr_dir = os.getcwd()
+    main(dirs,curr_dir)
